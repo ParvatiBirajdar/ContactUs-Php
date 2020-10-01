e9ceb48af599d57273b72dbaa84bfbd4605b841a
Parvati <birajdarparvati13@gmail.com>
2020-10-01 10:58:35 +0530

    changes

--- index.php
+++ index.php
@@ -3,14 +3,16 @@ if(!empty($_POST["send"])) {
 	$name = $_POST["userName"];
 	$email = $_POST["userEmail"];
 	$content = $_POST["content"];
+ 
+	$conn = mysqli_connect("localhost", "root", "", "test") or die("Connection Error: " . mysqli_error($conn));
+	
+	// host name, db user name, db pwd default(null),db name
 
-	$conn = mysqli_connect("localhost", "root", "", "") or die("Connection Error: " . mysqli_error($conn));
 	mysqli_query($conn, "INSERT INTO tblcontact (user_name, user_email,content) VALUES ('" . $name. "', '" . $email. "','" . $content. "')");
+
 	$insert_id = mysqli_insert_id($conn);
-	//if(!empty($insert_id)) {
 	   $message = "Your contact information is saved successfully.";
 	   $type = "success";
-	//}
 }
 require_once "contact-view.php";
 ?>
